d8018a325443b7ae5f01fbec2b32577cf4fc78bb
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-04-12 15:51:08 +0200

    feat: content ContactSection + new sass vard

--- src/components/ContactSection/ContactInfo/ContactInfo.jsx
+++ src/components/ContactSection/ContactInfo/ContactInfo.jsx
@@ -3,7 +3,15 @@ import './ContactInfo.scss';
 const ContactInfo = () => {
     return (
         <div className="contact__info">
-            email et telephone
+            <p>
+                <i className="fa-solid fa-location-dot"></i> 22, rue de la moisson 54000 MUIZON
+            </p>
+            <p>
+                [icone mail] cueillette-oharvest@mail.com
+            </p>
+            <p>
+                [icone telephone] 00 00 00 00 00
+            </p>
         </div>
     );
 }
--- src/components/ContactSection/Horaire/Horaire.jsx
+++ src/components/ContactSection/Horaire/Horaire.jsx
@@ -3,7 +3,46 @@ import './Horaire.scss';
 const Horaire = () => {
     return (
         <div className="contact__horaire">
-            les horaires
+            <h5>[icone montre] Les horaires</h5>
+            <table>
+                <tbody>
+                    <tr>
+                        <td className='contact__horaire-jour'>Lundi</td>
+                        <td>fermé</td>
+                        <td>13h00-18hh</td>
+                    </tr>
+                    <tr>
+                        <td className='contact__horaire-jour'>Mardi</td>
+                        <td>9h00-12h00</td>
+                        <td>13h00-18hh</td>
+                    </tr>
+                    <tr>
+                        <td className='contact__horaire-jour'>Mercredi</td>
+                        <td>9h00-12h00</td>
+                        <td>13h00-18hh</td>
+                    </tr>
+                    <tr>
+                        <td className='contact__horaire-jour'>Jeudi</td>
+                        <td>9h00-12h00</td>
+                        <td>13h00-18hh</td>
+                    </tr>
+                    <tr>
+                        <td className='contact__horaire-jour'>Vendredi</td>
+                        <td>9h00-12h00</td>
+                        <td>13h00-18hh</td>
+                    </tr>
+                    <tr>
+                        <td className='contact__horaire-jour'>Samedi</td>
+                        <td>9h00-12h00</td>
+                        <td>13h00-18hh</td>
+                    </tr>
+                    <tr>
+                        <td className='contact__horaire-jour'>Dimanche</td>
+                        <td>9h00-12h00</td>
+                        <td>13h00-18hh</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
