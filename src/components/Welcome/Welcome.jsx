bd235478c01ad76477dc4f00b173cf6eaf032257
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-04-12 15:50:13 +0200

    feat: add content Welcome component

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -5,9 +5,13 @@ import './Welcome.scss';
  */
 const Welcome = () => {
     return (
-        <section className='welcome'>
-            Welcome to O'Harvest!;
-        </section>
+        <div className='welcome'>
+            <img className='welcome__img' src='https://cdn.pixabay.com/photo/2018/11/10/11/03/pumpkin-3806393_960_720.jpg' alt='legumes'></img>
+            
+            <h2 className='welcome__title'>A propos de la cueillette</h2>
+
+            <p className='welcome__description'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum amet, mollitia recusandae quisquam consequuntur facere quos. Sint dolores voluptates eius iure molestiae recusandae eligendi eveniet commodi repellat, optio ullam odio.</p>
+        </div>
     );
 };
 
