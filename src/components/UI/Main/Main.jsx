4c8e7b553f9ae43bdbcb8b0accbf5754a1114c65
Mickael <magnin.mickael@outlook.fr>
2023-04-24 16:32:19 +0200

    merge: FormPage create (#17)
    * feat: create page about us
    
    * feat: create form page
    
    * feat: update

--- src/components/UI/Main/Main.jsx
+++ src/components/UI/Main/Main.jsx
@@ -3,6 +3,7 @@ import CropPage from '../../CropPage/CropPage';
 import HomePage from '../../HomePage/HomePage';
 import ProductPage from '../../ProductPage/ProductPage';
 import ProductsListPage from '../../ProductsListPage/ProductsListPage';
+import FormPage from '../../FormPage/FormPage';
 import ReservationPage from '../../ReservationPage/ReservationPage';
 import NotFound from '../../NotFound/NotFound';
 import { Routes, Route } from 'react-router-dom';
@@ -17,6 +18,7 @@ const Main = () => {
                 <Route path="/map" element={<CropPage />} />
                 <Route path="/products/:id" element={<ProductPage />} />
                 <Route path="/products" element={<ProductsListPage />} />
+                <Route path="/reservation/inscription" element={<FormPage />} />
                 <Route path="/reservation" element={<ReservationPage />} />
                 <Route path='*' element={<NotFound />} />
             </Routes>
---
+++ src/components/Who/Who.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Who.scss';
+import image from '../../assets/team.png';
+
+const Who = () => {
+
+    return (
+        <div className='who' >
+            <h2 className='who__title'>Qui sommes-nous ?</h2>
+            <img className='who__img' src={image} alt='' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, vel quod! Quidem consequuntur accusamus commodi voluptate tenetur incidunt illum molestias, dolor, voluptates aperiam ipsa aliquam voluptatibus sunt maxime fugiat cumqueTotam eligendi asperiores amet veritatis, nam cumque iure ratione qui nihil hic magnam vel ipsum officiis natus minus est obcaecati earum placeat non numquam excepturi. Accusamus labore laborum molestias ullam Magni deleniti quo laborum vero odit dignissimos alias aliquam odio debitis est reiciendis</p>
+        </div>
+    );
+};
+
+export default Who;
