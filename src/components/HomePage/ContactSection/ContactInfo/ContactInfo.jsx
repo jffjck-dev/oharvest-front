fadc064217325209e1db5936c7575fab8fe10b1e
Jean-François <79118746+jffjck-dev@users.noreply.github.com>
2023-05-03 08:50:53 +0200

    merge: update style and content modal AboutUs (#37)
    * fix: update ContactSection style -> OpeningHours + ContactInfo
    
    * feat: avatar add on assets
    
    * fix: update icon color on ContactSection
    
    * fix: update AboutUs componant-> avatar + scss + Button hover style
    
    * fix: update px -> rem
    
    * fix: text center -> on text hover
    
    * fix: rem conversion
    
    ---------
    
    Co-authored-by: NahashCode <a.vignard@gmail.com>

--- src/components/HomePage/ContactSection/ContactInfo/ContactInfo.jsx
+++ src/components/HomePage/ContactSection/ContactInfo/ContactInfo.jsx
@@ -12,13 +12,13 @@ const ContactInfo = () => {
             <h5 className="contact__info-title">CONTACTEZ-NOUS</h5>
             <ul className="contact__info-list">
                 <li className="contact__info-list-item">
-                    <i className="fa-solid fa-location-dot"></i> 27, rue du Lièvre 01940 ACME
+                    <i className="fa-solid fa-location-dot"></i> <span className="contact__info-list-item-title">Venir nous rencontrer :</span> 27, rue du Lièvre 01940 ACME
                 </li>
                 <li className="contact__info-list-item">
-                    <a href="mailto:accueil@oharvest.fr"><i className="fa-solid fa-envelope"></i> accueil@oharvest.fr</a>
+                    <a href="mailto:accueil@oharvest.fr"><i className="fa-solid fa-envelope"></i> <span className="contact__info-list-item-title" >Nous contacter par mail :</span> accueil@oharvest.fr</a>
                 </li>
                 <li className="contact__info-list-item">
-                    <a href="tel:0891650605"><i className="fa-solid fa-phone"></i> 08 91 650 605</a>
+                    <a href="tel:0891650605"><i className="fa-solid fa-phone"></i> <span className="contact__info-list-item-title">Pour nous appeler :</span> 08 91 650 605</a>
                 </li>
             </ul>
         </div>
