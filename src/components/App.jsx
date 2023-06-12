eb4fa43b4d1276434331e705fe0f51098633b7e2
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-06-12 02:27:13 +0200

    doc: add JsDoc comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Header from './Header/Header';
 import NavBar from './NavBar/NavBar';
 import Main from './UI/Main/Main';
@@ -7,6 +6,10 @@ import Footer from './Footer/Footer';
 
 import './App.scss';
 
+/**
+ * Root Element
+ * @returns {JSX.Element}
+ */
 function App() {
     return (
         <div className="app">
--- src/components/UI/Map/Map.jsx
+++ src/components/UI/Map/Map.jsx
@@ -4,6 +4,13 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 
 import './Map.scss';
 
+/**
+ * Reusable leaflet-react element to display plan on web page
+ * @param position {array<number>} latitude and longitude to center the map on loading component
+ * @param children {array<object>} any leaflet-react node
+ * @param zoom {number} map zoom define on loading component
+ * @returns {JSX.Element}
+ */
 const Map = ({position, children, zoom=13}) => {
     return (
         <MapContainer center={position} zoom={zoom} scrollWheelZoom={false} className="map">
