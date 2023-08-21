1675c9fc8ea86bf218ce733e6653550fba25ccfc
jffjck-dev <jeanfrancoiso.oclock@gmail.com>
2023-08-21 23:50:31 +0200

    fix(Modal): navigation keyboard and focus

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
-import Button from '../button/Button.jsx';
 
-import './Modal.scss';
+import classes from './Modal.module.css';
 
-const Modal = ({isModalOpen, closeModal, children}) => {
+/**
+ * Modal Component to display content as a modal window.
+ * @param {Object} props - The component props.
+ * @param {boolean} props.isModalOpen - Indicates whether the modal is open.
+ * @param {function} props.closeModal - Function to close the modal.
+ * @param {ReactNode} props.children - Content to be displayed inside the modal.
+ * @returns {ReactNode|null} - The modal or null if it's not open.
+ */
+const Modal = ({ isModalOpen, closeModal, children }) => {
+
+    // for closing modal with keyboard navigation
+    const handleKeyDown = (event) => {
+        if (event.key === 'Escape') {
+            closeModal();
+        }
+    };
+
+    // Create a portal to render the modal content into the body of the DOM, outside the component hierarchy
     return createPortal(
-        isModalOpen && <div className="overlay" onClick={closeModal}>
-            <div className="modal">
-                {children}
-                <Button modifier={'close'} text={'Fermer'} onClick={closeModal}/>
-            </div>
-        </div>,
+        // The overlay helps hide the background content and make the modal focusable
+        isModalOpen && (
+            <button
+                className={classes.overlay}
+                aria-label="Close Modal"
+                onClick={closeModal}
+                onKeyDown={handleKeyDown}
+            >
+                <div className={classes.modal}>
+                    {children}
+                </div>
+            </button>
+        ),
+        // Attach the modal to the body to ensure proper rendering
         document.body
     );
 };
